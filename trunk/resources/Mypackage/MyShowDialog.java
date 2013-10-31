7ff21e860cb8341f3f6854de6d2e5f81806e74c2
David.Biskupek@googlemail.com <David.Biskupek@googlemail.com@215a6852-3e29-d891-8dbb-3ec1dc764e46>
2013-10-31 22:08:42 +0000

    initial commit

---
+++ trunk/resources/Mypackage/MyShowDialog.java
@@ -0,0 +1,18 @@
+package Mypackage;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david
+ * Date: 23.10.13
+ * Time: 11:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyShowDialog extends AnAction {
+  @Override
+  public void actionPerformed(AnActionEvent anActionEvent) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+}
